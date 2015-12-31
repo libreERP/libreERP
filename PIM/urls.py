a0b0e6d6bb42d4fdfe9c5baa57a7d58becc905cd
Pradeep Kumar Yadav <ppyadav@sumtotalsystems.com>
2015-12-31 11:42:13 +0530

    minor

--- PIM/urls.py
+++ PIM/urls.py
@@ -13,6 +13,7 @@ router.register(r'settings' , settingsViewSet , base_name = 'settings')
 router.register(r'theme' , themeViewSet , base_name = 'theme')
 router.register(r'notification' , notificationViewSet, base_name = 'notification')
 router.register(r'chatMessage' , chatMessageViewSet, base_name = 'chatmessage')
+router.register(r'calendar' , calendarViewSet , base_name = 'calendar')
 router.register(r'chatMessageBetween' , chatMessageBetweenViewSet, base_name = 'chatbetween')
 
 urlpatterns = [
