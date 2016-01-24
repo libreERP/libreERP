050a7215549ad75f756cd256eaa7aff8a49e17d0
Pradeep Kumar Yadav <ppyadav@sumtotalsystems.com>
2016-01-24 13:00:05 +0530

    offering form

--- ecommerce/static/js/app.ecommerce.offerings.js
+++ ecommerce/static/js/app.ecommerce.offerings.js
@@ -1,7 +1,26 @@
+app.directive('ecommerceOfferingEditor', function () {
+  return {
+    templateUrl: '/static/ngTemplates/app.ecommerce.vendor.form.offering.html',
+    restrict: 'A',
+    replace: true,
+    scope: {
+      configObj:'@',
+    },
+    controller : 'ecommerce.form.listing',
+  };
+});
 
-app.controller('businessManagement.ecommerce.offerings' , function($scope , $http , $aside , $state, Flash , $users , $filter , $permissions){
-
+app.controller('ecommerce.form.listing' , function($scope , $state , $stateParams , $http , Flash){
   $scope.data = {mode : 'select' , form : {} };
+  $scope.config = JSON.parse($scope.configObj);
+
+  if (angular.isDefined($scope.config.pk)) {
+    $scope.id = $scope.config.pk;
+    $scope.editorMode = 'edit';
+  }else {
+    $scope.editorMode = 'new';
+    $scope.data.listing = $scope.config.parent;
+  }
 
   $scope.resetForm = function(){
     $scope.data.form = {
@@ -17,26 +36,6 @@ app.controller('businessManagement.ecommerce.offerings' , function($scope , $htt
 
   $scope.resetForm()
 
-  $scope.listingSearch = function(query) {
-    return $http.get('/api/ecommerce/listing/?title__contains=' + query).
-    then(function(response){
-      return response.data;
-    })
-  };
-
-  $scope.buildForm = function(){
-    $scope.data.mode = 'create'
-  }
-
-  var views = [{name : 'table' , icon : 'fa-bars' , template : '/static/ngTemplates/genericTable/tableDefault.html'},
-    ];
-
-  $scope.config = {
-    url : '/api/ecommerce/offeringAdmin/',
-    views : views,
-    searchField : 'item',
-  }
-
 
   $scope.submit = function() {
     dataToSend = $scope.data.form;
@@ -50,9 +49,37 @@ app.controller('businessManagement.ecommerce.offerings' , function($scope , $htt
     });
   }
 
+});
+app.controller('businessManagement.ecommerce.offerings' , function($scope , $http , $aside , $state, Flash , $users , $filter , $permissions){
+
+  $scope.getConfig = function(mode , data){
+    toReturn = {};
+    toReturn[mode] = data
+    return JSON.stringify(toReturn)
+  }
+
+  $scope.data = {mode : 'select' , form : {} };
+
+  var views = [{name : 'table' , icon : 'fa-bars' , template : '/static/ngTemplates/genericTable/tableDefault.html'},
+    ];
+
+  $scope.config = {
+    url : '/api/ecommerce/offeringAdmin/',
+    views : views,
+    searchField : 'item',
+  }
+
   $scope.goBack = function(){
     $scope.data.mode = 'select';
   }
+
+
+  $scope.listingSearch = function(query) {
+    return $http.get('/api/ecommerce/listing/?title__contains=' + query).
+    then(function(response){
+      return response.data;
+    })
+  };
 
 
 })
