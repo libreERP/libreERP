a18958128353a7cf30ecf56e9cea2ed3b2495e4c
Shubham Yadav <shubhamy2015@gmail.com>
2016-02-07 03:42:45 +0530

    changing service to partners. Now access vendors login from /partners

--- libreERP/urls.py
+++ libreERP/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from homepage.views import index
 from HR.views import loginView , logoutView , home , registerView , tokenAuthentication
-from ecommerce.views import ecommerceHome , serviceRegistration
+from ecommerce.views import ecommerceHome , partnerRegistration
 
 urlpatterns = [
     url(r"^$", ecommerceHome , name = 'index'),
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login', loginView , name ='login'),
     url(r'^register', registerView , name ='register'),
-    url(r'^providers', serviceRegistration , name ='serviceRegistration'),
+    url(r'^partners', partnerRegistration , name ='partnerRegistration'),
     url(r'^token', tokenAuthentication , name ='tokenAuthentication'),
     url(r'^logout/', logoutView , name ='logout'),
     url(r'^ERP/', home , name ='ERP'),
