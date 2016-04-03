dd7bf274cc8f358d5e736d38b45869cf0ef7abab
root <root@vps.goryd.com>
2016-04-03 07:23:32 +0000

    git setup script

--- sketch.py
+++ sketch.py
@@ -1,18 +1,20 @@
 from fabric.api import *
 
-# local('adduser git')
-# local('cat /etc/passwd');
-# local('ls /home/')
-# local('su - git')
+#local('adduser git')
+#local('cat /etc/passwd');
+#local('ls /home/')
+#local('su - git')
 # local('pwd')
+#local('git config --global user.name "git"')
+#local('git config --global user.email "git@libreERP"')
 
-# local('ssh-keygen')
+local('ssh-keygen')
 
-# local('mkdir -p ~/bin')
-# local('git clone git://github.com/sitaramc/gitolite')
-# local('gitolite/install -ln ~/bin')
-# local('cp ./.ssh/id_rsa.pub ./admin.pub')
-# local('./bin/gitolite setup -pk admin.pub')
+local('mkdir -p ~/bin')
+local('git clone git://github.com/sitaramc/gitolite')
+local('gitolite/install -ln ~/bin')
+local('cp ~/.ssh/id_rsa.pub ~/admin.pub')
+local('~/bin/gitolite setup -pk admin.pub')
 
 local('ssh git@localhost info')
 local('git clone git@localhost:gitolite-admin')
