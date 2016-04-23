963fa51d1bbea7d1ed2199c73bf32f0ecc8889cb
Pradeep Yadav <yadav.kumar@outlook.com>
2016-04-23 16:20:33 +0530

    line number view in code diff and comment box

--- gitweb/static/js/app.GIT.js
+++ gitweb/static/js/app.GIT.js
@@ -98,6 +98,27 @@ app.controller('projectManagement.GIT.default' , function($scope , $http , $asid
 
 app.controller('projectManagement.GIT.exploreNotification' , function($scope, $http , input) {
   $scope.commit = input;
+  $scope.mouseOver = {file  : -1 , line : -1};
+  $scope.commentEditor = {file  : -1 , line : -1};
+  $scope.lineNums = {}
+
+  $scope.mouseOn = function(fileIndex , lineIndex) {
+    $scope.mouseOver = {file : fileIndex , line : lineIndex};
+  };
+
+  $scope.mouseLeave = function(fileIndex , lineIndex) {
+    if ($scope.mouseOver.file == fileIndex && $scope.mouseOver.line == lineIndex) {
+      $scope.mouseOver = {file : -1 , line : -1};
+    }
+  };
+
+  $scope.addComment = function(fileIndex , lineIndex) {
+    if ($scope.commentEditor.file == fileIndex && $scope.commentEditor.line == lineIndex) {
+      $scope.commentEditor = {file  : -1 , line : -1};
+    }else {
+      $scope.commentEditor = {file : fileIndex , line : lineIndex};
+    }
+  };
 
   var params = {
     repo : $scope.commit.repo.pk,
@@ -117,6 +138,40 @@ app.controller('projectManagement.GIT.exploreNotification' , function($scope, $h
   $http({method : 'GET' , url : '/api/git/browseRepo/' , params : params }).
   then(function(response) {
     $scope.data = response.data;
+    for (var i = 0; i < $scope.data.diffs.length; i++) {
+      var f = $scope.data.diffs[i]; // file
+      var lines = f.diff.split('\n');
+      var leftStartsWith;
+      var leftLineNums;
+      var rightStartsWith;
+      var rightLineNums;
+      var lineNums = [['' , ''],['' , '']];
+      for (var j = 2; j < lines.length; j++) {
+        var l = lines[j];
+        if ((l.match(/@@/g)||[]).length == 2){
+          l = l.replace(/@@/g,'').replace(/-/g,'').replace(/\+/g,'');
+          var parts = l.split(' ');
+          leftStartsWith = parseInt(parts[1].split(',')[0]);
+          leftLineNums = parseInt(parts[1].split(',')[1]);
+          rightStartsWith = parseInt(parts[2].split(',')[0]);
+          rightLineNums = parseInt(parts[2].split(',')[1]);
+          lineNums.push(['' , ''])
+        }else {
+          if (l[0]=='-') {
+            lineNums.push([leftStartsWith , ''])
+            leftStartsWith += 1;
+          }else if (l[0] == '+') {
+            lineNums.push(['' , rightStartsWith])
+            rightStartsWith += 1;
+          }else  {
+            lineNums.push([leftStartsWith , rightStartsWith])
+            rightStartsWith += 1;
+            leftStartsWith += 1;
+          }
+        }
+      }
+      $scope.lineNums[f.path] = lineNums;
+    }
   });
 
 
