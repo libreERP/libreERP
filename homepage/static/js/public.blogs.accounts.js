48706ca4c9b956076427e7ff388485fff65f1095
Pradeep Kumar Yadav <51613174@HCLT.CORP.HCL.IN>
2016-08-03 13:21:03 +0530

    angular module for accounts

---
+++ homepage/static/js/public.blogs.accounts.js
@@ -0,0 +1,18 @@
+var app = angular.module('app' , ['ui.bootstrap', 'ngSanitize', 'ngTagsInput' ]);
+
+app.config(function( $httpProvider ){
+
+  $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+  $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+  $httpProvider.defaults.withCredentials = true;
+
+});
+
+app.controller('public.blogs.accounts' , function($scope, $http){
+
+  console.log('loaded');
+
+  $scope.testVar = 'Dsdasd'
+
+
+});
