c2b2eca868824190a65f9d80632c125b7de4fb2e
Pradeep Yadav <pkyisky@gmail.com>
2016-08-04 09:20:29 +0530

    password and profile update in blogs

--- blogs/views.py
+++ blogs/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets , permissions , serializers
 from django.shortcuts import render
 from url_filter.integrations.drf import DjangoFilterBackend
+from django.contrib import messages
+from django.contrib.auth import authenticate
 from API.permissions import *
 from PIM.models import *
+from HR.models import profile
 
 def blogs(request):
     print 'home'
@@ -32,14 +35,61 @@ def savedView(request):
 
 
 def accountsView(request):
-    print 'home'
+    usrProfile = profile.objects.get(user = request.user)
+    if request.method == 'POST':
+        if request.GET['page'] == 'profile' :
+            if 'dp' in request.FILES:
+                dp = request.FILES['dp']
+                usrProfile.displayPicture = dp
+            if 'email' in request.POST:
+                eml = request.POST['email']
+                usrProfile.email = eml
+            usrProfile.save()
+            messages.success(request, "Profile updated successfully!")
+        elif request.GET['page'] == 'password':
+            p = request.POST['oldpassword']
+            p1 = request.POST['password1']
+            p2 = request.POST['password2']
+            if p1 != p2:
+                messages.error(request, "New password and confirm password does not match!")
+            if len(p) == 0:
+                messages.error(request, "Please provide the current password!")
+            user = request.user
+            if p1 == p2 and authenticate(username = user.username , password = p) is not None:
+                user.set_password(p1)
+                user.save()
+                messages.success(request, "Password updated successfully!")
+    if 'page' in request.GET:
+        tab = request.GET['page']
+        if tab == 'profile':
+            profileTab = True
+            passwordTab = False
+        else:
+            profileTab = False
+            passwordTab = True
+    else:
+        passwordTab = False
+        profileTab = False
     if request.user.is_anonymous():
         totalContribution = 0
         contributedArticles = []
     else:
         contributedArticles = request.user.articles.all()
         totalContribution = contributedArticles.count()
-    return render(request , 'blogs.accounts.html', {'totalContribution' : totalContribution , 'contributedArticles' : contributedArticles})
+
+    if profile.objects.get(user = request.user).displayPicture=='':
+        DPSrc = '/static/images/userIcon.png'
+    else:
+        DPSrc = usrProfile.displayPicture.url
+    # messages.error(request, "Huge success!")
+    # messages.info(request, "Huge success!")
+    ctx = {'totalContribution' : totalContribution ,
+        'contributedArticles' : contributedArticles,
+        'DPSrc' : DPSrc,
+        'email' : usrProfile.email,
+        'profileTab' : profileTab,
+        'passwordTab' : passwordTab}
+    return render(request , 'blogs.accounts.html', ctx)
 
 def searchView(request):
     key = request.POST['key']
