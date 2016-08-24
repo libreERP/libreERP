c8fefd6b1afd8269d4bfc68723a573a4caf32106
pradeep <pkyisky@gmail.com>
2016-08-24 19:49:37 +0530

    minor

--- virtualWorkforce/static/js/app.virtualWorkforce.controlRoom.js
+++ virtualWorkforce/static/js/app.virtualWorkforce.controlRoom.js
@@ -1,6 +1,37 @@
 app.controller('businessManagement.virtualWorkforce.controlRoom' , function($scope , $http , $aside , $state, Flash , $users , $filter , $permissions , $sce){
 
 
+  $scope.data = {tableData : null};
+
+
+  var views = [{name : 'table' , icon : 'fa-bars' , template : '/static/ngTemplates/genericTable/tableDefault.html'},
+  ];
+
+  $scope.config = {
+    views : views,
+    url : '/api/virtualWorkforce/process/',
+    fields : ['name' , 'description' , 'created' ],
+    searchField: 'name',
+    itemsNumPerView : [12,24,48],
+  }
+
+
+  $scope.tableAction = function(target , action , mode){
+    console.log(target , action , mode);
+    console.log($scope.data.tableData);
+    if (action=='edit') {
+      for (var i = 0; i < $scope.data.tableData.length; i++) {
+        if ($scope.data.tableData[i].pk == parseInt(target)){
+
+
+
+
+        }
+      }
+    }
+  }
+
+
 
 
 });
